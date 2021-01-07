78d900686b2f08789fc0e1201ec9b2594dbca31c
mzakocs <mzakocs@gmail.com>
2021-01-07 13:46:33 -0700

    Added aliases, meeting link, and advanced options

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 // AutoFill fields in the options page
-chrome.storage.sync.get(["user", "class"], (result) => {
+chrome.storage.sync.get(["user", "class", "meeting", "alias", "gmcu"], (result) => {
   // Starts with user info
   if (result.user !== undefined) {
     let userOptions = result.user;
@@ -8,13 +8,35 @@ chrome.storage.sync.get(["user", "class"], (result) => {
       field.value = userOptions[fieldName];
     }
   }
-  // Then does class info
+  // Fills out the attendance links
   if (result.class !== undefined) {
     let classOptions = result.class;
     for (fieldName of Object.keys(classOptions)) {
-      let field = document.getElementById("period" + fieldName.toString());
+      let field = document.getElementById(`period${fieldName.toString()}_attendance`);
       field.value = classOptions[fieldName].link;
     }
+  }
+  // Fills out the meeting links
+  if (result.meeting !== undefined) {
+    let meetingOptions = result.meeting;
+    for (fieldName of Object.keys(meetingOptions)) {
+      let field = document.getElementById(`period${fieldName.toString()}_meeting`);
+      field.value = meetingOptions[fieldName].link;
+    }
+  }
+  // Fills out the aliases
+  if (result.alias !== undefined) {
+    let aliasOptions = result.alias;
+    for (aliasNum of Object.keys(aliasOptions)) {
+      let field = document.getElementById(`period${aliasNum.toString()}_alias`);
+      field.value = aliasOptions[aliasNum];
+    }
+  }
+  // Fills out the Google Meets Code Utility section
+  if (result.gmcu !== undefined) {
+    let gmcuLink = result.gmcu;
+    let field = document.getElementById("googleMeetBaseLink");
+    field.value = gmcuLink;
   }
 });
 
@@ -37,20 +59,86 @@ userOptionsSave.addEventListener("click", function () {
   });
 });
 
-// Save button for class options
-let classOptionsSave = document.getElementById("classOptionsSave");
-classOptionsSave.addEventListener("click", function() {
-  // Grab the values of the fields
-  let classValues = {};
+// Save Button for Attendance Link Options
+let attendanceLinkSave = document.getElementById("attendanceLinkSave");
+attendanceLinkSave.addEventListener("click", function() {
+  // Grab the values of the attendance link fields
+  let attendanceValues = {};
   for (let i = 0; i < 10; i++) {
-    let classValue = document.getElementById("period" + i.toString()).value;
-    if (classValue) {
-      classValues[i] = {period_num: i, link: classValue, last_completed: null}
+    let attendanceValue = document.getElementById(`period${i.toString()}_attendance`).value;
+    if (attendanceValue) {
+      attendanceValues[i] = {period_num: i, link: attendanceValue, last_completed: null}
     }
   }
   // Stores them in sync storage
-  chrome.storage.sync.set({class: classValues}, function() {
-    alert("Saved Class Options!");
-    console.log(classValues)
+  chrome.storage.sync.set({class: attendanceValues}, function() {
+    alert("Saved Attendance Link Options!");
   });
 });
+
+// Save Button for Meeting Link Options
+let meetingLinkSave = document.getElementById("meetingLinkSave");
+meetingLinkSave.addEventListener("click", function() {
+  // Grab the values of the meeting link fields
+  let meetingValues = {};
+  for (let i = 0; i < 10; i++) {
+    let meetingValue = document.getElementById(`period${i.toString()}_meeting`).value;
+    if (meetingValue) {
+      meetingValues[i] = {period_num: i, link: meetingValue}
+    }
+  }
+  // Stores them in sync storage
+  chrome.storage.sync.set({meeting: meetingValues}, function() {
+    alert("Saved Meeting Link Options!");
+  });
+});
+
+// Save Button for Class Aliases
+let aliasSave = document.getElementById("aliasSave");
+aliasSave.addEventListener("click", function() {
+  // Grab the values of the alias fields
+  let aliasValues = {};
+  for (let i = 0; i < 10; i++) {
+    let aliasValue = document.getElementById(`period${i.toString()}_alias`).value;
+    if (aliasValue) {
+      aliasValues[i] = aliasValue;
+    }
+  }
+  // Stores them in sync storage
+  chrome.storage.sync.set({alias: aliasValues}, function() {
+    alert("Saved Class Alias Options!");
+  });
+});
+
+// Save Button for Google Meets Code Utility
+let gmcuSave = document.getElementById("gmcuSave");
+gmcuSave.addEventListener("click", function() {
+  // Grab the values of gmcu field
+  let gmcuField = document.getElementById("googleMeetBaseLink");
+  let gmcuFieldValue = gmcuField.value;
+  // Stores them in sync storage
+  if (gmcuFieldValue.includes("meet.google.com")) {
+    chrome.storage.sync.set({gmcu: gmcuFieldValue}, function() {
+      alert("Saved Class Alias Options!");
+    });
+  }
+  else {
+    alert("Not a Valid Google Meets Base Link! Please enter a URL that has a base URL of meet.google.com.");
+  }
+  
+});
+
+// Sets up the accordions for advanced options
+let accordions = document.getElementsByClassName("accordion");
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    // Hides or un-hides the accordion
+    if (panel.style.display === "flex") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "flex";
+    }
+  });
+}
--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
-// Sets up a control for each period that's been given
-let periodsContainer = document.getElementById("periodsContainer");
-// Function Declaration
+/* FUNCTION DECLARATIONS */
+// Updates the date of a attendance auto-fill
 const updateDate = function (periodNumber) {
   // Gets the old period data
   chrome.storage.sync.get(["class"], (result) => {
@@ -9,10 +8,11 @@ const updateDate = function (periodNumber) {
     chrome.storage.sync.set({ class: data });
   });
 };
+
+// This grabs the user info, opens the given form, and then injects
+// the "formFillInjection" function into the google sheets page with the given params
+// Starts by getting the user info from the storage
 const filloutForm = async function () {
-  // This grabs the user info, opens the given form, and then injects
-  // the "formFillInjection" function into the google sheets page with the given params
-  // Starts by getting the user info from the storage
   let { period_num, last_completed, link } = this;
   await chrome.storage.sync.get(["user"], (result) => {
     if (result.user === undefined) return;
@@ -49,17 +49,40 @@ const filloutForm = async function () {
       });
     });
   });
+  // Changes the icon on the popup menu
+  let completedIcon = document.getElementById(`period${period_num}_icon`);
+  completedIcon.className = "fa fa-check green";
 };
-// Gets each of the setup classes
-chrome.storage.sync.get(["class"], (result) => {
+
+// Opens the meeting link from a button
+const openMeetingLink = async function () {
+  let { period_num, link } = this;
+  chrome.tabs.update(null, { url: link, active: false });
+};
+
+/* RUNNING CODE STARTS HERE */
+// Sets up a control for each period that's been given
+let periodsContainer = document.getElementById("periodsContainer");
+
+// Gets each of the attendance link objects from sync storage
+chrome.storage.sync.get(["class", "meeting", "alias"], (result) => {
   if (result.class !== undefined) {
     let classOptions = result.class;
-    // Goes through each of the options and adds a control cluster
+    // Goes through each of the options and adds a control cluster for each in the popup
     for (periodNumber of Object.keys(classOptions)) {
       let period = classOptions[periodNumber];
       // Adds the main period control div
       let periodControl = document.createElement("div");
-      periodControl.innerHTML = "Period " + periodNumber.toString();
+      let periodControlLabel = document.createElement("p");
+      // Sets an alias label if there is one set
+      if (result.alias && result.alias[periodNumber]) {
+        periodControlLabel.innerHTML = result.alias[periodNumber];
+      }
+      else {
+        periodControlLabel.innerHTML = "Period " + periodNumber.toString();
+      }
+      periodControlLabel.className = "periodLabel";
+      periodControl.appendChild(periodControlLabel);
       periodControl.className = "periodControls container";
       periodsContainer.appendChild(periodControl);
       // Adds autofill button to the period control div
@@ -67,19 +90,21 @@ chrome.storage.sync.get(["class"], (result) => {
       let autoFillButton = document.createElement("button");
       let autoFillIcon = document.createElement("i");
       let completedIcon = document.createElement("i");
+      // Presets it to uncompleted (red x)
       completedIcon.className = "fa fa-times red";
       completedIcon.title = "Not Completed!";
+      completedIcon.id = `period${periodNumber}_icon`;
       if (period.last_completed !== undefined) {
         let lastCompleted = new Date(period?.last_completed);
         if (lastCompleted.getDate() === new Date().getDate()) {
-          // Has been completed today
+          // Has been completed today, show a green check
           completedIcon.className = "fa fa-check green";
           completedIcon.title = "Completed!";
         }
       }
       // Does some styling on the icon and buttons
       autoFillIcon.className = "fa fa-clipboard";
-      autoFillButton.title = "Auto-Fill Attendance";
+      autoFillButton.title = "Auto-Fill Attendance for Period " + periodNumber;
       // Adds a click listener to the autofill button
       console.log(period);
       autoFillButton.addEventListener("click", filloutForm.bind(period));
@@ -88,6 +113,22 @@ chrome.storage.sync.get(["class"], (result) => {
       periodControlButtonPanel.appendChild(completedIcon);
       periodControlButtonPanel.appendChild(autoFillButton);
       periodControl.appendChild(periodControlButtonPanel);
+      // Adds a meeting link to the periodControlButtonPanel if it exists
+      if (
+        result.meeting !== undefined &&
+        result.meeting[periodNumber] !== undefined
+      ) {
+        let meetingLinkButton = document.createElement("button");
+        let meetingLinkButtonIcon = document.createElement("i");
+        meetingLinkButton.title = "Open Meeting for Period " + periodNumber;
+        meetingLinkButtonIcon.className = "fa fa-tv";
+        meetingLinkButton.appendChild(meetingLinkButtonIcon);
+        meetingLinkButton.addEventListener(
+          "click",
+          openMeetingLink.bind(result.meeting[periodNumber])
+        );
+        periodControlButtonPanel.appendChild(meetingLinkButton);
+      }
     }
     // Adds a button at the bottom of the page that fills out every form instantly
     // let allButton = document.createElement('button');
@@ -107,6 +148,6 @@ chrome.storage.sync.get(["class"], (result) => {
   // Adds an info section to the bottom of the page
   let infoSection = document.createElement("h5");
   infoSection.innerText =
-    'To configure the extension, right click on the menu button and press "Options"';
+    'To configure the extension, right-click on the MP Logo and press "Options"';
   document.body.appendChild(infoSection);
 });
